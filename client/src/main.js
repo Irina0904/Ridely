6858793bc9fe1774f80ff7dfa06ccee294dec435
Irina0904 <71022317+Irina0904@users.noreply.github.com>
2021-10-15 12:29:38 +0200

    Added the icons package

--- client/src/main.js
+++ client/src/main.js
@@ -4,11 +4,16 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import VueGeolocation from 'vue-browser-geolocation'
 import * as VueGoogleMaps from 'vue2-google-maps'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faParking, faStore, faFilter, faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+library.add(faParking, faStore, faFilter, faBars)
 Vue.use(VueGeolocation)
 Vue.use(VueGoogleMaps, {
   load: {
